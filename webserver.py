cabf18a8925b1d5da2f051e14dd07a0bc9afb7a8
qwemicheal <qwemicheal@gmail.com>
2017-03-21 17:10:53 +0800

    add gunicorn main

--- webserver.py
+++ webserver.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 from logger import root
 @app.route('/')
@@ -12,5 +13,11 @@ def hello_world():
 @app.route('/test_json',methods=['POST'])
 def test_json():
     print request.get_json()
+    print request.data
     root.info("testing ")
     return jsonify('test jsoninfo')
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0", port=5000, debug=False)
